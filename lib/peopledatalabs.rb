96462ddf3c426780f987563e04c2d6b0b12c4cb4
vvillait88 <vvillait88@yahoo.com>
2024-03-20 12:22:01 -0700

    feat: Bump version to 3.0.0 and remove beta flag from autocomplete

--- lib/peopledatalabs.rb
+++ lib/peopledatalabs.rb
@@ -15,7 +15,7 @@ require 'peopledatalabs/resources/skill'
 
 
 # gem build peopledatalabs.gemspec
-# gem install ./peopledatalabs-2.3.0.gem
+# gem install ./peopledatalabs-3.0.0.gem
 # irb
 # require 'peopledatalabs'
 # rake spec PDL_API_KEY=API_KEY
