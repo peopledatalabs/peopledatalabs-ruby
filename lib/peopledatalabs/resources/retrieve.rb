4e4d67753bb26f2ee5b9daa222cc24fc0afb7729
Varun Villait <vvillait88@yahoo.com>
2024-09-17 08:24:50 -0700

    Support for Updated Title Roles (#23)
    * api updates
    
    * add tests
    
    * fixes

--- lib/peopledatalabs/resources/retrieve.rb
+++ lib/peopledatalabs/resources/retrieve.rb
@@ -2,12 +2,12 @@
 
 module Peopledatalabs
   class Retrieve < APIResource
-    def self.person(person_id:, pretty: false)
+    def self.person(person_id:, pretty: false, updated_title_roles: false)
       headers = {
         'Accept-Encoding' => 'gzip',
         'User-Agent' => 'PDL-RUBY-SDK',
       }
-      get(path: "/v5/person/retrieve/#{person_id}", headers: headers, params: { 'pretty' => pretty })
+      get(path: "/v5/person/retrieve/#{person_id}", headers: headers, params: { 'pretty' => pretty, 'updated_title_roles' => updated_title_roles })
     end
   end
 end
--- lib/peopledatalabs/resources/search.rb
+++ lib/peopledatalabs/resources/search.rb
@@ -2,27 +2,29 @@
 
 module Peopledatalabs
   class Search < APIResource
-    def self.person(searchType:, query: , titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil)
+    def self.person(searchType:, query: , titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil, updated_title_roles: false)
       search(searchType: searchType,
              query: query,
              titlecase: titlecase,
              dataset: dataset, size: size,
              pretty: pretty,
              scroll_token: scroll_token,
+             updated_title_roles: updated_title_roles,
              kind: 'person')
     end
 
-    def self.company(searchType:, query: , titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil)
+    def self.company(searchType:, query: , titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil, updated_title_roles: false)
       search(searchType: searchType,
              query: query,
              titlecase: titlecase,
              dataset: dataset, size: size,
              pretty: pretty,
              scroll_token: scroll_token,
+             updated_title_roles: updated_title_roles,
              kind: 'company')
     end
 
-    def self.search(searchType:, query:, kind:, titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil)
+    def self.search(searchType:, query:, kind:, titlecase: false, dataset: 'all', size: 10, pretty: false, scroll_token: nil, updated_title_roles: false)
 
       body = {
         searchType === 'sql' ? 'sql' : 'query' => query,
@@ -30,7 +32,8 @@ module Peopledatalabs
         'size' => size,
         'pretty' => pretty,
         'titlecase' => titlecase,
-        'scroll_token' => scroll_token
+        'scroll_token' => scroll_token,
+        'updated_title_roles' => updated_title_roles
       }
 
       headers = {
